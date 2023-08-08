8850f14cbdf224494fd5713d5f1b6b5ee3e23dcc
Shidqi Akram Hauzan <57066616+Wafelkertas@users.noreply.github.com>
2023-08-08 10:13:18 +0700

    Initial commit

---
+++ app/src/main/java/com/shidqi/childapplicationview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shidqi.childapplicationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
